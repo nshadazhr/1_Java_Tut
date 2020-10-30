c35066cb6e536b91327187c7dc4dccb306d72010
rahul gupta <rahul.gupta.webcoir@gmail.com>
2020-10-30 17:05:29 +0530

    init commit

---
+++ app/src/main/java/com/example/a1_java_tut/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a1_java_tut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public static void main( ){System.out.println( " hello" );
+    }
+}
+
